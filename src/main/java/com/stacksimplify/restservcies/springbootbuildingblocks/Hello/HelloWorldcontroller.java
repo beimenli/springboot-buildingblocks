7af49d51f87dbfff11f40e44d4be84a52b17bd1f
Sam Zhang <Sam.Zhang@rci.rogers.ca>
2019-10-19 19:52:16 -0400

    Hello world Base

---
+++ src/main/java/com/stacksimplify/restservcies/springbootbuildingblocks/Hello/HelloWorldcontroller.java
@@ -0,0 +1,21 @@
+package com.stacksimplify.restservcies.springbootbuildingblocks.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldcontroller {
+	
+	//@RequestMapping(method = RequestMethod.GET,path = "/helloworld")
+	@GetMapping("/helloworld1")
+	public String helloWorld() {
+		
+		return "Hello World";
+	}
+	
+    @GetMapping("/helloworld-bean")
+    public UserDetails helloWorldBean() {
+        return new UserDetails("Kalyan","Reddy","Hyderabad");
+    }
+
+}
